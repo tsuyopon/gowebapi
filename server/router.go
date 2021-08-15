db1ea5b92d143b14132bf316bb37085cab60e37b
Tsuyoshi Ooishi <tsuyoshiooishi@yahoo.co.jp>
2021-08-15 09:43:11 +0900

    add javascript and css

--- server/router.go
+++ server/router.go
@@ -13,16 +13,24 @@ func GetRouter() *gin.Engine {    // *gin.Engineの表記は返り値の型
     router := gin.Default()
 
     router.LoadHTMLGlob("view/**/*.html")  // 全体に適用したい場合(view/common, view/sourceへの適用が必要なため)
-//  router.Static("/assets", "./assets")
+
+    // 静的なassetsを配置する場所
+    router.Static("/images", "./assets/images")
+    router.Static("/css", "./assets/css")
+    router.Static("/js", "./assets/js")
+
+    // web/v1グループ
+    webv1:= router.Group("/web/v1")
+    {
+        webv1.GET("/", v1_top.IndexDisplayAction)
+        webv1.GET("/user", v1_user.UserListDisplayAction)
+        webv1.GET("/user/add", v1_user.UserAddDisplayAction)
+        webv1.GET("/user/edit/:id", v1_user.UserEditDisplayAction)
+    }
 
     // /api/v1グループ分離
     apiv1 := router.Group("/api/v1")
     {
-        apiv1.GET("/", v1_top.IndexDisplayAction)
-        apiv1.GET("/user", v1_user.UserListDisplayAction)
-        apiv1.GET("/user/add", v1_user.UserAddDisplayAction)
-        apiv1.GET("/user/edit/:id", v1_user.UserEditDisplayAction)
-
         // util
         apiv1.GET("/clientip", util.DisplayClientIPAddressAction)
         apiv1.GET("/ping", util.DisplayPingAction)
